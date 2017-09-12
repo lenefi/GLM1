82989362a1c96b151a37ab1f346f95de0ffdcf03
lenefi <lenefi@stud.ntnu.no>
2017-09-12 13:49:11 +0200

    Add files via upload
    Lagt til ggpairs

--- mylm/R/main.R
+++ mylm/R/main.R
@@ -7,3 +7,10 @@ data(SLID, package = "car")
 SLID <- SLID[complete.cases(SLID), ]
 
 summary(SLID)
+lm(SLID)
+
+install.packages("GGally")
+install.packages("ggplot2")
+
+library("GGally")
+ggpairs(SLID, columns = 1:5)
